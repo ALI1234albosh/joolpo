4d3a9c7a956f8c7ccb213fce8a9ea5c7cb617e06
ALI1234albosh <139685975+ALI1234albosh@users.noreply.github.com>
2023-07-16 16:37:30 +0530

    Add files via upload

---
+++ Online course registartion/onlinecourse/admin/includes/footer.php
@@ -0,0 +1,10 @@
+<footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    &copy; 2020 Online Course Management System By : <a href="http://www.phpgurukul.com/" target="_blank">Mohemmed</a>
+                </div>
+
+            </div>
+        </div>
+    </footer>
